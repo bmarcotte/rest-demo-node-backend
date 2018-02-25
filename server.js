741b782b1d569140a476b7dc498fb317ef6998e7
Ben Marcotte <33464129+bmarcotte@users.noreply.github.com>
2018-02-24 18:48:00 -0800

    Added server.js and startup configs
    Added a simple server.js file to startup express and confirm that it is
    running.  Added entries to "scripts" in package.json for commands to
    start it up in dev or prod ("npm run start-dev" and "npm run start"/
    "npm start", respectively).

---
+++ server.js
@@ -0,0 +1,18 @@
+const express     = require( 'express' );
+const body_parser = require( 'body-parser' );
+
+/* global process */
+
+const app = express();
+// Support parsing of application/x-www-form-urlencoded post data
+app.use( body_parser.urlencoded( { extended: true } ) );
+// Support parsing of application/json type post data
+app.use( body_parser.json() );
+
+app.get( '/', function ( req, res ) {
+  res.json( { message: 'express server is up and responding.' } );
+} );
+
+const port = process.env.PORT || 8080;
+app.listen( port );
+console.warn( 'Listening on port: ' + port );
