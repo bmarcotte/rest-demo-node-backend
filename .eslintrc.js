23a499399e2464a506edfa60a177ee9d2db9d5fc
Ben Marcotte <33464129+bmarcotte@users.noreply.github.com>
2018-02-24 18:13:56 -0800

    Installed eslint
    Installed eslint module, and added a configuration file for it,
    .eslintrc.js.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+      "browser": false,
+      "es6": true,
+      "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-console": [
+          "error",
+          { "allow": [ "warn", "error" ] }
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
