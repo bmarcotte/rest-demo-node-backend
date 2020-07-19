6d6df249377505eefe453778a86e3a814066dc09
Ben Marcotte <33464129+bmarcotte@users.noreply.github.com>
2020-07-19 12:31:32 -0700

    Updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-    "env": {
-      "browser": false,
-      "es6": true,
-      "node": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "no-console": [
-          "error",
-          { "allow": [ "warn", "error" ] }
-        ],
-        "quotes": [
-            "error",
-            "single",
-            { "avoidEscape": true }
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  'env': {
+    'browser': false,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'no-console': [
+      'error',
+      { 'allow': [ 'warn', 'error' ] }
+    ],
+    'quotes': [
+      'error',
+      'single',
+      { 'avoidEscape': true }
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
